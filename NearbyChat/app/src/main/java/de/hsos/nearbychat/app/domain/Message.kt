22a02449b29d3805054584b4253e0bab28712f52
timlock <81235388+timlock@users.noreply.github.com>
2023-01-03 12:18:29 +0100

    Domainlayer angelegt

---
+++ NearbyChat/app/src/main/java/de/hsos/nearbychat/app/domain/Message.kt
@@ -0,0 +1,5 @@
+package de.hsos.nearbychat.app.domain
+
+data class Message(val senderMAC: String, val receiverMAC: String, val content: String, val timeStamp: String) {
+
+}
--- NearbyChat/app/src/main/java/de/hsos/nearbychat/app/domain/Profile.kt
+++ NearbyChat/app/src/main/java/de/hsos/nearbychat/app/domain/Profile.kt
@@ -1,4 +1,9 @@
 package de.hsos.nearbychat.app.domain
 
-class Profile {
+data class Profile(val macAddress: String) {
+    private lateinit var name: String
+    private lateinit var description: String
+    private var isAvailable: Boolean = false
+    private var neighbors: List<Profile> = mutableListOf<Profile>()
+    private var rssi: Int = 0
 }
