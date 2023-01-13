dff0e3381ec4bff90db6662df53b910d1e571aff
FreshGarry <41427332+FreshGarry@users.noreply.github.com>
2023-01-13 17:02:23 +0100

    Nachrichten eindeutig durch Adresse & Zeitstempel

--- NearbyChat/app/src/main/java/de/hsos/nearbychat/app/domain/Message.kt
+++ NearbyChat/app/src/main/java/de/hsos/nearbychat/app/domain/Message.kt
@@ -3,10 +3,8 @@ package de.hsos.nearbychat.app.domain
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(primaryKeys = ["address", "timeStamp"])
 class Message(val address: String, val content: String, val timeStamp: Long) {
-    @PrimaryKey(autoGenerate = true)
-    var id: Long = 0
     var isReceived: Boolean = false
     var isSelfAuthored: Boolean = false
 }
