909c2a3b9c4a4fc7a5d7af265961fca108571fd4
FreshGarry <41427332+FreshGarry@users.noreply.github.com>
2023-01-15 16:14:22 +0100

    Permissions & Eingeschaltete Dienste zum Start eingebaut und gefordert

--- NearbyChat/app/src/main/java/de/hsos/nearbychat/app/view/SplashScreenActivity.kt
+++ NearbyChat/app/src/main/java/de/hsos/nearbychat/app/view/SplashScreenActivity.kt
@@ -1,33 +1,50 @@
 package de.hsos.nearbychat.app.view
 
 import android.Manifest
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import de.hsos.nearbychat.R
 
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val TAG: String = ViewModel::class.java.simpleName
+
+    private lateinit var bluetoothManager: BluetoothManager
+    private lateinit var locationManager: LocationManager
+
+    private var alreadyTriedPermissionRequest = false
+
+    private var btIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        proceedCheck()
+    }
+
+    private var gpsIntentLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        proceedCheck()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         supportActionBar?.hide()
 
-        if(!checkPermissions()) {
-            return
-        }
-        if(!checkCompatibility()) {
-            return
-        }
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        proceedCheck()
     }
 
     override fun onRequestPermissionsResult(
@@ -37,26 +54,101 @@ class SplashScreenActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        for(result in grantResults) {
-            if(result != PackageManager.PERMISSION_GRANTED) {
-                findViewById<TextView>(R.id.splash_screen_text).setText(R.string.permissions_missing)
-                return
-            }
-        }
-        if(!checkCompatibility()) {
+        alreadyTriedPermissionRequest = true
+
+        proceedCheck()
+    }
+
+    private fun proceedCheck() {
+        if(!checkPermissions()) {
+            requestMissingPermissions()
             return
         }
+        if(!checkBtCompatibility()) {
+            return
+        }
+        if(!checkBtEnabled()) {
+            requestBtEnable()
+            return
+        }
+        if(!checkBtLeCompatibility()) {
+            return
+        }
+        if(!checkGpsEnabled()) {
+            requestGpsEnable()
+            return
+        }
+        startApp()
+    }
+
+    private fun checkPermissions() : Boolean {
+        return getMissingPermissions().isEmpty()
+    }
+
+    private fun requestMissingPermissions() {
+        Log.d(TAG, "requestMissingPermissions: ")
+        if(!alreadyTriedPermissionRequest) {
+            requestPermissions(getMissingPermissions().toTypedArray(), 0)
+        } else {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle(R.string.request_permissions)
+            builder.setCancelable(false)
+            builder.setPositiveButton(R.string.ok) { _, _ ->
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri: Uri = Uri.fromParts("package", packageName, null)
+                intent.data = uri
+                startActivity(intent)
+            }
+            builder.show()
+        }
+    }
+
+    private fun checkBtCompatibility(): Boolean {
+        return bluetoothManager.adapter != null
+    }
+
+    private fun checkBtEnabled(): Boolean {
+        return bluetoothManager.adapter.isEnabled
+    }
+
+    private fun requestBtEnable() {
+        Log.d(TAG, "requestBtEnable: ")
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        btIntentLauncher.launch(enableBtIntent)
+    }
+
+    private fun checkBtLeCompatibility(): Boolean {
+        return bluetoothManager.adapter.isLe2MPhySupported && bluetoothManager.adapter.isLeCodedPhySupported
+    }
+
+    private fun checkGpsEnabled(): Boolean {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    private fun requestGpsEnable() {
+        Log.d(TAG, "requestGpsEnable: ")
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(R.string.request_gps)
+        builder.setCancelable(false)
+        builder.setPositiveButton(R.string.ok) { _, _ ->
+            val enableGpsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            gpsIntentLauncher.launch(enableGpsIntent)
+        }
+        builder.show()
+    }
+
+    private fun startApp() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
     }
 
-    private fun checkPermissions() : Boolean {
+    private fun getMissingPermissions(): List<String> {
         val permissionNeeded = mutableListOf(
             Manifest.permission.BLUETOOTH,
             Manifest.permission.BLUETOOTH_ADMIN,
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
+            Manifest.permission.ACCESS_COARSE_LOCATION,
         )
 
         if(Build.VERSION.SDK_INT > 30) {
@@ -72,20 +164,6 @@ class SplashScreenActivity : AppCompatActivity() {
                 missingPermissions.add(permission)
             }
         }
-        if (missingPermissions.size > 0) {
-            requestPermissions(missingPermissions.toTypedArray(), 0)
-            return false
-        }
-        return true
-    }
-
-    private fun checkCompatibility(): Boolean {
-        val bluetoothManager: BluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-
-        if(bluetoothManager.adapter.isLe2MPhySupported && bluetoothManager.adapter.isLeCodedPhySupported) {
-            return true;
-        }
-        findViewById<TextView>(R.id.splash_screen_text).setText(R.string.not_compatible)
-        return false
+        return missingPermissions
     }
 }
