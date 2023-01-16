3444127af1ff48bac388e6121b60c2d1dc7de8e8
FreshGarry <41427332+FreshGarry@users.noreply.github.com>
2023-01-16 16:49:20 +0100

    Datenbank änderung: Message hat verbesserten Primary Key

--- NearbyChat/app/src/main/java/de/hsos/nearbychat/app/domain/Message.kt
+++ NearbyChat/app/src/main/java/de/hsos/nearbychat/app/domain/Message.kt
@@ -3,7 +3,7 @@ package de.hsos.nearbychat.app.domain
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(primaryKeys = ["address", "timeStamp"])
+@Entity(primaryKeys = ["address", "timeStamp", "isSelfAuthored"])
 class Message(val address: String, val content: String, val timeStamp: Long) {
     var isReceived: Boolean = false
     var isSelfAuthored: Boolean = false
