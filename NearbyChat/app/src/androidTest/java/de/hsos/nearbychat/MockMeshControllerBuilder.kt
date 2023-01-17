19f739dc49623274d4ac907243547ee6ddb4696d
timlock <81235388+timlock@users.noreply.github.com>
2023-01-17 14:02:45 +0100

    Tests für den Meshcontroller

---
+++ NearbyChat/app/src/androidTest/java/de/hsos/nearbychat/MockMeshControllerBuilder.kt
@@ -0,0 +1,70 @@
+package de.hsos.nearbychat
+
+import de.hsos.nearbychat.app.domain.OwnProfile
+import de.hsos.nearbychat.service.bluetooth.Advertiser
+import de.hsos.nearbychat.service.bluetooth.MeshController
+import de.hsos.nearbychat.service.bluetooth.MeshObserver
+import de.hsos.nearbychat.service.bluetooth.Scanner
+import de.hsos.nearbychat.service.bluetooth.scan.ScannerObserver
+import de.hsos.nearbychat.service.bluetooth.util.Advertisement
+import de.hsos.nearbychat.service.bluetooth.util.AdvertisementPackage
+
+class MockMeshControllerBuilder private constructor() {
+    private var ownProfile: OwnProfile? = null
+    private var sizeLimit: Int? = null
+    private var send: ((String) -> Unit)? = null
+
+    fun ownProfile(name: String): MockMeshControllerBuilder {
+        this.ownProfile = OwnProfile(name)
+        return this
+    }
+
+    fun advertiser(sizeLimit: Int, send: (String) -> Unit): MockMeshControllerBuilder {
+        this.sizeLimit = sizeLimit
+        this.send = send
+        return this
+    }
+
+
+    fun build(meshObserver: MeshObserver): MeshController {
+        lateinit var scannerObserver: ScannerObserver
+        val advertiser: Advertiser = object : Advertiser {
+            override fun start(): Boolean {
+                return true
+            }
+
+            override fun stop() {
+            }
+
+            override fun getMaxMessageSize(): Int {
+                return this@MockMeshControllerBuilder.sizeLimit!!
+            }
+
+            override fun send(message: String): Boolean {
+                this@MockMeshControllerBuilder.send!!(message)
+                return true
+            }
+
+        }
+        val scanner: Scanner = object : Scanner {
+            override fun start(): Boolean {
+                return true
+            }
+
+            override fun stop(): Boolean {
+                return true
+            }
+
+            override fun subscribe(observer: ScannerObserver) {
+                scannerObserver = observer
+            }
+
+        }
+        return  MeshController(meshObserver, advertiser, scanner,this@MockMeshControllerBuilder.ownProfile!!)
+    }
+
+
+    companion object {
+        fun createBuilder(): MockMeshControllerBuilder = MockMeshControllerBuilder()
+    }
+}
